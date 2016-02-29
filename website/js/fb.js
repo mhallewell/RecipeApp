fcb664f0d4d7d5732aa6e161adc2854ada5e17bd
AxiosAmnesia <tannan1964@hotmail.com>
2016-02-29 10:22:44 -0700

    added fb login

---
+++ website/js/fb.js
@@ -0,0 +1,50 @@
+var accessToken = "null";
+var userData;
+var userEmail;
+
+window.fbAsyncInit = function() {
+	FB.init({
+		appId      : '224587191222975',
+		xfbml      : true,
+		version    : 'v2.5'
+	});
+};
+
+
+
+(function(d, s, id){
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) {return;}
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/en_US/sdk.js";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function login()
+{
+	FB.login(function(response)
+	{
+		if(response.status === 'connected')
+		{
+			document.getElementById('status').innerHTML = 'Connected';
+			accessToken = response.authResponse.accessToken;
+			FB.api('/me', function(response) 
+			{
+				userData = response;
+				document.getElementById('token').innerHTML = JSON.stringify(userData, null, 4);
+				FB.api(('/me/' + userData.id + '?fields=cover'), function(response) {
+					userCover = response.cover;
+					document.createElement('img').appendChild += JSON.stringify(userCover, null, 4);
+				});
+			});
+		}
+		else if (response.status === 'not_authorized')
+		{
+			document.getElementById('status').innerHTML = 'not connected';
+		}
+		else
+		{
+			document.getElementById('status').innerHTML = 'not connected at all';
+		}
+	})
+}
