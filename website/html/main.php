898845eec7eb9a2805d9797d652a2706c8c15e78
MARY HALLEWELL <mhallewell@gmail.com>
2016-03-21 09:40:30 -0600

    changed .html extension to .php

---
+++ website/html/main.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+      <link rel="stylesheet" type="text/css" href="../css/stylesheet.css">
+</head>
+<body>
+
+  <div id="header">
+
+    <a class="logout" href="login.html"><img src="http://dabuttonfactory.com/button.png?t=Logout&f=Caviar-Bold&ts=20&tc=eef5db&hp=20&vp=8&c=1&bgt=gradient&bgc=fe5f55&ebgc=fe5f55"/></a>
+    <img src="../image/recipeHeader.png" width="400" height="200"/>
+    <!-- <h1>Recipe Calendar</h1> -->
+    <div id="menu">
+      <ul>
+        <li><a href="main.html"><img src="../image/Utensil.png" width="60" height="100"/></a></li>
+        <li><a href="calendar.html"><img src="http://dabuttonfactory.com/button.png?t=Calendar&f=Caviar-Bold&ts=28&tc=eef5db&hp=20&vp=8&c=1&bgt=gradient&bgc=fe5f55&ebgc=fe5f55"/></a></li>
+        <li><a href="recipes.html"><img src="http://dabuttonfactory.com/button.png?t=Recipes&f=Caviar-Bold&ts=28&tc=eef5db&hp=20&vp=8&c=1&bgt=gradient&bgc=fe5f55&ebgc=fe5f55"/></a></li>
+        <li><a href="shoppinglist.html"><img src="http://dabuttonfactory.com/button.png?t=Shopping+List&f=Caviar-Bold&ts=28&tc=eef5db&hp=20&vp=8&c=1&bgt=gradient&bgc=fe5f55&ebgc=fe5f55"/></a></li>
+        <li><a href="pantry.html"><img src="http://dabuttonfactory.com/button.png?t=Pantry&f=Caviar-Bold&ts=28&tc=eef5db&hp=20&vp=8&c=1&bgt=gradient&bgc=fe5f55&ebgc=fe5f55"/></a></li>
+        <li><a href="profilepage.html"><img src="http://dabuttonfactory.com/button.png?t=Profile&f=Caviar-Bold&ts=28&tc=eef5db&hp=20&vp=8&c=1&bgt=gradient&bgc=fe5f55&ebgc=fe5f55"/></a></li>
+      </ul>
+    </div>
+  </div>
+
+</body>
+</html>
