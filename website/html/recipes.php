8df6353023747142563c7aa9f73d52d3f0817fcc
MARY HALLEWELL <mhallewell@gmail.com>
2016-04-04 17:00:52 -0600

    actually edit extension for recipe search page

---
+++ website/html/recipes.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="../css/stylesheet.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="../js/header.js"></script>
+</head>
+<body>
+
+  <div id="header">
+    <!-- <h1>Recipe Calendar</h1> -->
+  </div>
+
+  <div class="dropdown">
+    <p>Search Type</p>
+    <div class="dropdown-content">
+      <a href="TODO">All</a>
+      <a href="TODO">Breakfast</a>
+      <a href="TODO">Lunch</a>
+      <a href="TODO">Dinner</a>
+      <a href="TODO">Appetizer</a>
+      <a href="TODO">Dessert</a>
+    </div>
+  <form>
+    Search for Recipe: <input type="text" name="firstname">
+  </form>
+  </div>
+</body>
+</html>
