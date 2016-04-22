7ef743e2e9b0687ded3ffe317bcf633929265bea
AxiosAmnesia <tannan1964@hotmail.com>
2016-04-22 11:20:19 -0600

    quick fix

--- website/html/createRecipe.php
+++ website/html/createRecipe.php
@@ -4,6 +4,7 @@
 <head>
       <link rel="stylesheet" type="text/css" href="../css/stylesheet.css">
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+      <script src="../js/main.js"></script>
       <script src="../js/header.js"></script>
 </head>
 <body>
