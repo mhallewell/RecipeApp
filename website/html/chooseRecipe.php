36f6383870b10797b0d99c28fa1971ed125689a0
troenoel <noelle.troestersolbrig@aggiemail.usu.edu>
2016-04-25 11:24:00 -0600

    add recipe link

---
+++ website/html/chooseRecipe.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="../css/stylesheet.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="../js/header.js"></script>
+</head>
+<body>
+
+  <div id="header">
+    <!-- <h1>Recipe Calendar</h1> -->
+
+  </div>
+
+  <div id="viewAllRecipesBody">
+    <ul>
+	<?php foreach ($recipes as $recipe)
+		{
+			echo '<li><a href="/addRecipeToDate.php?id=';
+			echo $recipe->getId();
+			echo '&date=';
+      echo $date->format('Y-m-d');
+      echo'">';
+			echo strip_tags($recipe->getName());
+			echo '</a></li>';
+		}
+ 		?>
+    </ul>
+  </div>
